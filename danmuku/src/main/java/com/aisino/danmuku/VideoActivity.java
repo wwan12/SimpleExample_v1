0425e9fd18c3a6e73ad2c54c9e9a6150fb9cf7fa
yhq <376954839@qq.com>
2018-08-29 17:01:44 +0800

    v1.1.1

---
+++ danmuku/src/main/java/com/aisino/danmuku/VideoActivity.java
@@ -0,0 +1,45 @@
+package com.aisino.danmuku;
+
+/**
+ * 文件描述：
+ * 作者：Administrator
+ * 创建时间：2018/8/29/029
+ * 更改时间：2018/8/29/029
+ * 版本号：1
+ */
+public class VideoActivity {
+    //    2.Add JZVideoPlayer in your layout:
+//
+//    <cn.jzvd.JZVideoPlayerStandard
+//    android:id="@+id/videoplayer"
+//    android:layout_width="match_parent"
+//    android:layout_height="200dp"/>
+//    3.Set the video uri, video thumb url and video title:
+//
+//    JZVideoPlayerStandard jzVideoPlayerStandard = (JZVideoPlayerStandard) findViewById(R.id.videoplayer);
+//    jzVideoPlayerStandard.setUp("http://jzvd.nathen.cn/c6e3dc12a1154626b3476d9bf3bd7266/6b56c5f0dc31428083757a45764763b0-5287d2089db37e62345123a1be272f8b.mp4",
+//    JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL,
+//    "饺子闭眼睛");
+//    jzVideoPlayerStandard.thumbImageView.setImage("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640");
+//    4.In Activity:
+//
+//    @Override
+//    public void onBackPressed() {
+//        if (JZVideoPlayer.backPress()) {
+//            return;
+//        }
+//        super.onBackPressed();
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        JZVideoPlayer.releaseAllVideos();
+//    }
+//    5.In AndroidManifest.xml:
+//
+//    <activity
+//    android:name=".MainActivity"
+//    android:configChanges="orientation|screenSize|keyboardHidden"
+//    android:screenOrientation="portrait" />
+//    <!-- or android:screenOrientation="landscape"-->
+}
